b25e93861c73fe160f1286cb4f991c9bf019967c
HackerulGogu <g0atthesecond1@gmail.com>
2021-08-31 11:08:52 +0300

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        if len(self.body) > 100:
+            return str(self.body[:100] + '...')
+        return self.body
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
